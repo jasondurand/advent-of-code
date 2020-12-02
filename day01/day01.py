f680fc483855e860f4903b6c425ac6ff61a1cd71
Jason Durand <jasonadurand@gmail.com>
2020-12-01 21:41:31 -0700

    part2 completed

--- day01/day01.py
+++ day01/day01.py
@@ -10,3 +10,10 @@ for value1 in expenses:
 	for value2 in expenses:
 		if (value1+value2) == 2020:
 			print("value1: {:d}, value2: {:d}, product: {:d}".format(value1,value2,value1*value2))
+			
+# part2
+for value1 in expenses:
+	for value2 in expenses:
+		for value3 in expenses:
+			if (value1+value2+value3) == 2020:
+				print("v1: {:d}, v2: {:d}, v3: {:d}, prod: {:d}".format(value1,value2,value3,value1*value2*value3))
